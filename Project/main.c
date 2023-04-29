16c8d0d4d26d950b933db0aad4b986a7bcdb40c0
William65536 <110579495+William65536@users.noreply.github.com>
2023-04-29 09:28:41 -0400

    Add files via upload

--- Project/main.c
+++ Project/main.c
@@ -12,22 +12,61 @@
 /* TODO: Maybe use a LISP-like language internally or use a pseudo symbolic algebra range system to replace the current object-oriented system */
 /* TODO: Add parameterized episolids (episterea) */
 
-typedef double Mat4[4][4];
+typedef double Mat4[4][4]; /* TODO: Maybe make this a 3x4 matrix instead */
 
-void Mat4_invert(Mat4 self) /* TODO: Handle the case when a matrix is uninvertible */
-{
-    assert(self != NULL);
-    assert(*self != NULL);
-}
-
-Ray Mat4_mult_ray(Mat4 self, Ray ray) /* TODO: Maybe `ray` should be modified to prevent needless copying */
+static void Mat4_invert(Mat4 self)
 {
     assert(self != NULL);
     assert(*self != NULL);
 
-    Ray temp;
+    const double denom =
+        self[0][0] *
+            (self[1][1] * (self[2][2] * self[3][3] - self[2][3] * self[3][2]) 
+           + self[1][2] * (self[2][3] * self[3][1] - self[2][1] * self[3][3])
+           + self[1][3] * (self[2][1] * self[3][2] - self[2][2] * self[3][1]))
+      + self[0][1] *
+            (self[1][0] * (self[2][3] * self[3][2] - self[2][2] * self[3][3])
+           + self[1][2] * (self[2][0] * self[3][3] - self[2][3] * self[3][0])
+           + self[1][3] * (self[2][2] * self[3][0] - self[2][0] * self[3][2]))
+      + self[0][2] *
+            (self[1][0] * (self[2][1] * self[3][3] - self[2][3] * self[3][1])
+           + self[1][1] * (self[2][3] * self[3][0] - self[2][0] * self[3][3])
+           + self[1][3] * (self[2][0] * self[3][1] - self[2][1] * self[3][0]))
+      + self[0][3] *
+            (self[1][0] * (self[2][2] * self[3][1] - self[2][1] * self[3][2])
+           + self[1][1] * (self[2][0] * self[3][2] - self[2][2] * self[3][0])
+           + self[1][2] * (self[2][1] * self[3][0] - self[2][0] * self[3][1]));
 
-    return temp;
+    assert(denom != 0.0);
+
+    const Mat4 temp = {
+        {
+            (self[1][1] * (self[2][2] * self[3][3] - self[2][3] * self[3][2]) + self[1][2] * (self[2][3] * self[3][1] - self[2][1] * self[3][3]) + self[1][3] * (self[2][1] * self[3][2] - self[2][2] * self[3][1])) / denom,
+            (self[0][1] * (self[2][3] * self[3][2] - self[2][2] * self[3][3]) + self[0][2] * (self[2][1] * self[3][3] - self[2][3] * self[3][1]) + self[0][3] * (self[2][2] * self[3][1] - self[2][1] * self[3][2])) / denom,
+            (self[0][1] * (self[1][2] * self[3][3] - self[1][3] * self[3][2]) + self[0][2] * (self[1][3] * self[3][1] - self[1][1] * self[3][3]) + self[0][3] * (self[1][1] * self[3][2] - self[1][2] * self[3][1])) / denom,
+            (self[0][1] * (self[1][3] * self[2][2] - self[1][2] * self[2][3]) + self[0][2] * (self[1][1] * self[2][3] - self[1][3] * self[2][1]) + self[0][3] * (self[1][2] * self[2][1] - self[1][1] * self[2][2])) / denom
+        },
+        {
+            (self[1][0] * (self[2][3] * self[3][2] - self[2][2] * self[3][3]) + self[1][2] * (self[2][0] * self[3][3] - self[2][3] * self[3][0]) + self[1][3] * (self[2][2] * self[3][0] - self[2][0] * self[3][2])) / denom,
+            (self[0][0] * (self[2][2] * self[3][3] - self[2][3] * self[3][2]) + self[0][2] * (self[2][3] * self[3][0] - self[2][0] * self[3][3]) + self[0][3] * (self[2][0] * self[3][2] - self[2][2] * self[3][0])) / denom,
+            (self[0][0] * (self[1][3] * self[3][2] - self[1][2] * self[3][3]) + self[0][2] * (self[1][0] * self[3][3] - self[1][3] * self[3][0]) + self[0][3] * (self[1][2] * self[3][0] - self[1][0] * self[3][2])) / denom,
+            (self[0][0] * (self[1][2] * self[2][3] - self[1][3] * self[2][2]) + self[0][2] * (self[1][3] * self[2][0] - self[1][0] * self[2][3]) + self[0][3] * (self[1][0] * self[2][2] - self[1][2] * self[2][0])) / denom
+        },
+        {
+            (self[1][0] * (self[2][1] * self[3][3] - self[2][3] * self[3][1]) + self[1][1] * (self[2][3] * self[3][0] - self[2][0] * self[3][3]) + self[1][3] * (self[2][0] * self[3][1] - self[2][1] * self[3][0])) / denom,
+            (self[0][0] * (self[2][3] * self[3][1] - self[2][1] * self[3][3]) + self[0][1] * (self[2][0] * self[3][3] - self[2][3] * self[3][0]) + self[0][3] * (self[2][1] * self[3][0] - self[2][0] * self[3][1])) / denom,
+            (self[0][0] * (self[1][1] * self[3][3] - self[1][3] * self[3][1]) + self[0][1] * (self[1][3] * self[3][0] - self[1][0] * self[3][3]) + self[0][3] * (self[1][0] * self[3][1] - self[1][1] * self[3][0])) / denom,
+            (self[0][0] * (self[1][3] * self[2][1] - self[1][1] * self[2][3]) + self[0][1] * (self[1][0] * self[2][3] - self[1][3] * self[2][0]) + self[0][3] * (self[1][1] * self[2][0] - self[1][0] * self[2][1])) / denom
+        },
+        {
+            (self[1][0] * (self[2][2] * self[3][1] - self[2][1] * self[3][2]) + self[1][1] * (self[2][0] * self[3][2] - self[2][2] * self[3][0]) + self[1][2] * (self[2][1] * self[3][0] - self[2][0] * self[3][1])) / denom,
+            (self[0][0] * (self[2][1] * self[3][2] - self[2][2] * self[3][1]) + self[0][1] * (self[2][2] * self[3][0] - self[2][0] * self[3][2]) + self[0][2] * (self[2][0] * self[3][1] - self[2][1] * self[3][0])) / denom,
+            (self[0][0] * (self[1][2] * self[3][1] - self[1][1] * self[3][2]) + self[0][1] * (self[1][0] * self[3][2] - self[1][2] * self[3][0]) + self[0][2] * (self[1][1] * self[3][0] - self[1][0] * self[3][1])) / denom,
+            (self[0][0] * (self[1][1] * self[2][2] - self[1][2] * self[2][1]) + self[0][1] * (self[1][2] * self[2][0] - self[1][0] * self[2][2]) + self[0][2] * (self[1][0] * self[2][1] - self[1][1] * self[2][0])) / denom
+        }
+    };
+
+    memcpy(self, temp, sizeof temp);
 }
 
 // typedef struct AnglePair { double theta, phi; } AnglePair;
@@ -40,6 +79,29 @@ typedef struct Vec3 { double x, y, z; } Vec3;
 typedef struct Ray {
     Vec3 pos, dpos;
 } Ray;
+
+static Vec3 Mat4_mult_Vec3(const Mat4 self, Vec3 vec)
+{
+    assert(self != NULL);
+    assert(*self != NULL);
+
+    return (Vec3) {
+        .x = self[0][0] * vec.x + self[0][1] * vec.y + self[0][2] * vec.z + self[0][3],
+        .y = self[1][0] * vec.x + self[1][1] * vec.y + self[1][2] * vec.z + self[1][3],
+        .z = self[2][0] * vec.x + self[2][1] * vec.y + self[2][2] * vec.z + self[2][3]
+    };
+}
+
+static Ray Mat4_mult_Ray(const Mat4 self, Ray ray) /* TODO: Maybe `ray` should be modified to prevent needless copying */
+{
+    assert(self != NULL);
+    assert(*self != NULL);
+
+    return (Ray) {
+        .pos = Mat4_mult_Vec3(self, ray.pos),
+        .dpos = Mat4_mult_Vec3(self, ray.dpos)
+    };
+}
 
 typedef struct Solid Solid;
 struct Solid {
@@ -55,6 +117,7 @@ Solid *Solid_primitive_new(Mat4 transformation, bool (*primitive_isintersect)(co
 {
     assert(transformation != NULL);
     assert(*transformation != NULL);
+    assert(primitive_isintersect != NULL);
 
     Solid *ret = malloc(sizeof *ret);
 
@@ -71,13 +134,21 @@ Solid *Solid_primitive_new(Mat4 transformation, bool (*primitive_isintersect)(co
     return ret;
 }
 
+Solid *Solid_composite_new(Solid *left, Solid *right)
+{
+    assert(left != NULL);
+    assert(right != NULL);
+
+    return NULL;
+}
+
 bool Solid_ellipsoid_isintersect(const Solid *this, Ray ray)
 {
     assert(this != NULL);
     assert(this->scene_transform.from != NULL);
     assert(*this->scene_transform.from != NULL);
 
-    Ray tray = Mat4_mult_ray(this->scene_transform.from, ray);
+    Ray tray = Mat4_mult_Ray(this->scene_transform.from, ray);
 
     double b = 2 * (tray.pos.x * tray.dpos.x + tray.pos.y * tray.dpos.y + tray.pos.z * tray.dpos.z);
     b *= b;
@@ -158,6 +229,31 @@ bool Solid_difference_isintersect(const Solid *this, Ray ray)
 
 int main(void)
 {
+    Mat4 mat = {
+        { 1.0, 0.0, 0.0, 5.0 },
+        { 0.0, 1.0, 0.0, 0.0 },
+        { 0.0, 0.0, 1.0, 0.0 },
+        { 0.0, 0.0, 0.0, 1.0 }
+    };
+
+    Vec3 orig = { .x = 0.0, .y = 0.0, .z = -10.0 };
+
+    Ray ray = {
+        .pos = orig,
+        .dpos = (Vec3) { .x = 0.0, .y = 0.0, .z = 0.0 }
+    };
+
+    Mat4_invert(mat);
+
+    Ray tray = Mat4_mult_Ray(mat, ray);
+
+    printf("[<%g %g %g> <%g %g %g>]\n", tray.pos.x, tray.pos.y, tray.pos.z, tray.dpos.x, tray.dpos.y, tray.dpos.z);
+
+    printf("[ %5g %5g %5g %5g ]\n", mat[0][0], mat[0][1], mat[0][2], mat[0][3]);
+    printf("[ %5g %5g %5g %5g ]\n", mat[1][0], mat[1][1], mat[1][2], mat[1][3]);
+    printf("[ %5g %5g %5g %5g ]\n", mat[2][0], mat[2][1], mat[2][2], mat[2][3]);
+    printf("[ %5g %5g %5g %5g ]\n", mat[3][0], mat[3][1], mat[3][2], mat[3][3]);
+
 
     return 0;
 }
